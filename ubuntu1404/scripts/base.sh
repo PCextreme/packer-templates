43fde2c3f47967aa280d22f1cc0cc46ce358d7d2
Wido den Hollander <wido@widodh.nl>
2014-06-02 21:21:10 +0200

    Various fixes to Ubuntu 14.04 template

--- ubuntu1404/scripts/base.sh
+++ ubuntu1404/scripts/base.sh
@@ -3,3 +3,6 @@
 echo "updating the machine"
 apt-get update
 apt-get -y upgrade
+
+echo "setting noatime option"
+sed -i 's|errors=remount-ro|errors=remount-ro,noatime|g' /etc/fstab
--- ubuntu1404/scripts/install-set-password-script.sh
+++ ubuntu1404/scripts/install-set-password-script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "installing cloud-set-guest-password"
-wget -O /etc/init.d/cloud-set-guest-password http://download.cloud.com/templates/4.2/bindir/cloud-set-guest-password.in
+wget -O /etc/init.d/cloud-set-guest-password http://crew.pcextreme.nl/files/linux/util/cloudstack/cloud-set-guest-password.in || exit 1
+
 chmod +x /etc/init.d/cloud-set-guest-password
 update-rc.d cloud-set-guest-password defaults
-
-
