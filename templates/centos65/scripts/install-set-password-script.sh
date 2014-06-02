c9a7572e83b9578dd682ab79379b5e36e6278aab
Wido den Hollander <wido@widodh.nl>
2014-06-02 22:34:02 +0200

    Sync templates by using the same scripts

--- templates/centos65/scripts/install-set-password-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "installing cloud-set-guest-password"
-wget -O /etc/init.d/cloud-set-guest-password http://download.cloud.com/templates/4.2/bindir/cloud-set-guest-password.in
-chmod +x /etc/init.d/cloud-set-guest-password
-chkconfig --add cloud-set-guest-password
-
-
--- templates/debian75/scripts/install-set-password-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "installing cloud-set-guest-password"
-wget -O /etc/init.d/cloud-set-guest-password http://download.cloud.com/templates/4.2/bindir/cloud-set-guest-password.in
-chmod +x /etc/init.d/cloud-set-guest-password
-sed -i '25i# Wait 5sec for network\nsleep 5\n' /etc/init.d/cloud-set-guest-password
-update-rc.d cloud-set-guest-password defaults
