3edbfcc3fa59ee579840458a7a27988aeec22c6e
Wido den Hollander <wido@pcextreme.nl>
2014-10-02 16:40:45 +0200

    Add a script to build all templates at once
    This can probably be improved a lot, but it's a start

---
+++ build-all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+TEMPLATES_DIR="${SCRIPT_DIR}/templates"
+
+PACKER=$(which packer)
+if [ -z "$PACKER" ]; then
+    echo "Error: Packer could not be found in PATH"
+    exit 1
+fi
+
+for TEMPLATE in $(find $TEMPLATES_DIR -maxdepth 1 -mindepth 1 -type d); do
+    cd $TEMPLATE
+    if [ ! -f "template.json" ]; then
+        echo "Warning: Could not find template.json in $TEMPLATE Skipping."
+        continue
+    fi
+
+    $PACKER build -force template.json
+    if [ "$?" -ne 0 ]; then
+        echo "Error: Failed to build $TEMPLATE"
+        continue
+    fi
+done
