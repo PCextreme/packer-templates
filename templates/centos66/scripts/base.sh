291d651f961c2d205e25fac7c2ec529e896cfa16
Lennert den Teuling <lennert@denteuling.com>
2014-11-06 17:15:00 +0100

    Upgraded CentOS 6.5 template to CentOS 6.6

--- templates/centos66/scripts/base.sh
+++ templates/centos66/scripts/base.sh
@@ -3,6 +3,10 @@
 echo "Update packages"
 yum -y update
 
+echo "installing cloud-set-guest-password"
+chmod +x /etc/init.d/cloud-set-guest-password
+chkconfig --add cloud-set-guest-password
+
 echo "installing cloud-set-guest-sshkey"
 chmod +x /etc/init.d/cloud-set-guest-sshkey
 chkconfig --add cloud-set-guest-sshkey
