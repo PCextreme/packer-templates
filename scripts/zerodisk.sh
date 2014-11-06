2135c4b9ed9b4d231af250cbaae98c6e52a851ce
Lennert den Teuling <lennert@denteuling.com>
2014-11-06 16:28:01 +0100

    Added new Ubuntu 14.10 packer template

---
+++ scripts/zerodisk.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Zero out the free space to save space"
+dd if=/dev/zero of=/EMPTY bs=1M
+sync
+rm -f /EMPTY
+sync
---
+++ templates/ubuntu1410/scripts/base.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "updating the machine"
+apt-get update
+apt-get -y dist-upgrade
+
+echo "installing cloud-set-guest-password"
+chmod +x /etc/init.d/cloud-set-guest-password
+update-rc.d cloud-set-guest-password defaults
+
+echo "installing cloud-set-guest-sshkey"
+chmod +x /etc/init.d/cloud-set-guest-sshkey
+update-rc.d cloud-set-guest-sshkey defaults
+
+echo "setting noatime option"
+sed -i 's|errors=remount-ro|errors=remount-ro,noatime|g' /etc/fstab
