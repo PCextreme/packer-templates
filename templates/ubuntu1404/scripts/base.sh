ec83d9451c2311c866c04229093f978dec36af50
Lennert den Teuling <lennert@denteuling.com>
2014-11-26 16:52:39 +0100

    Added cloud-init support for Ubuntu 14.04.1 LTS template. This automatically makes some bash script for keys and hostnames no longer needed.

--- templates/ubuntu1404/scripts/base.sh
+++ templates/ubuntu1404/scripts/base.sh
@@ -4,9 +4,9 @@ echo "updating the machine"
 apt-get update
 apt-get -y upgrade
 
-echo "installing cloud-set-guest-sshkey"
-chmod +x /etc/init.d/cloud-set-guest-sshkey
-update-rc.d cloud-set-guest-sshkey defaults
+echo "installing cloud-set-guest-password"
+chmod +x /etc/init.d/cloud-set-guest-password
+update-rc.d cloud-set-guest-password defaults
 
 echo "setting noatime option"
 sed -i 's|errors=remount-ro|errors=remount-ro,noatime|g' /etc/fstab
