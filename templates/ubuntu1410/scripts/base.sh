4fbef2022a7489992f9723659037cd633769eeff
Lennert den Teuling <lennert@denteuling.com>
2014-12-15 16:05:14 +0100

    Added cloud-init support for Ubuntu 14.10

--- templates/ubuntu1410/scripts/base.sh
+++ templates/ubuntu1410/scripts/base.sh
@@ -8,9 +8,5 @@ echo "installing cloud-set-guest-password"
 chmod +x /etc/init.d/cloud-set-guest-password
 update-rc.d cloud-set-guest-password defaults
 
-echo "installing cloud-set-guest-sshkey"
-chmod +x /etc/init.d/cloud-set-guest-sshkey
-update-rc.d cloud-set-guest-sshkey defaults
-
 echo "setting noatime option"
 sed -i 's|errors=remount-ro|errors=remount-ro,noatime|g' /etc/fstab
