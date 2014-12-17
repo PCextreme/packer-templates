b1378404088c5815decfddd4677cdb543b41968e
Lennert den Teuling <lennert@denteuling.com>
2014-12-17 16:42:32 +0100

    Added cloud-init support to CentOS 6.6 template

--- templates/centos66/scripts/base.sh
+++ templates/centos66/scripts/base.sh
@@ -7,9 +7,5 @@ echo "Install cloud-set-guest-password"
 chmod +x /etc/init.d/cloud-set-guest-password
 chkconfig --add cloud-set-guest-password
 
-echo "Install cloud-set-guest-sshkey"
-chmod +x /etc/init.d/cloud-set-guest-sshkey
-chkconfig --add cloud-set-guest-sshkey
-
 echo "Mount partitions with noatime attribute"
 sed -i '0,/defaults/{s/defaults/defaults,noatime/g}' /etc/fstab
--- templates/centos66/scripts/packages.sh
+++ templates/centos66/scripts/packages.sh
@@ -2,3 +2,5 @@
 
 echo "Install packages"
 yum -y groupinstall base
+yum -y install acpid ntp cloud-init
+chkconfig rdisc on
