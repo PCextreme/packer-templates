7d2b80781f60fc7e13762a07f337611bda0c8236
Lennert den Teuling <lennert@denteuling.com>
2014-12-17 12:36:52 +0100

    Added cloud-init support to 12.04.5 template

--- templates/ubuntu1204/scripts/base.sh
+++ templates/ubuntu1204/scripts/base.sh
@@ -3,15 +3,14 @@
 echo "updating the machine"
 rm -fR /var/lib/apt/lists/*
 apt-get update
-apt-get -y upgrade
+apt-get -y dist-upgrade
+
+echo "installing cloud-init"
+apt-get -y install cloud-init
 
 echo "installing cloud-set-guest-password"
 chmod +x /etc/init.d/cloud-set-guest-password
 update-rc.d cloud-set-guest-password defaults
-
-echo "installing cloud-set-guest-sshkey"
-chmod +x /etc/init.d/cloud-set-guest-sshkey
-update-rc.d cloud-set-guest-sshkey defaults
 
 echo "setting noatime option"
 sed -i 's|errors=remount-ro|errors=remount-ro,noatime|g' /etc/fstab
