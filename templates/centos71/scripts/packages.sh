498aa2b2df4412e1763d0daa188b31f121c8e09a
Wido den Hollander <wido@widodh.nl>
2015-05-01 11:45:01 +0200

    centos71: Install cloud-init from CentOS repository
    This version is not broken like the one in CentOS 7.0 and behaves
    properly with systemd.

--- templates/centos71/scripts/packages.sh
+++ templates/centos71/scripts/packages.sh
@@ -2,9 +2,5 @@
 
 echo "Installing Basic Server packages"
 yum -y groupinstall base
-yum -y install acpid ntp ndisc6
+yum -y install acpid ntp ndisc6 cloud-init
 chkconfig rdisc on
-
-# Install cloud-init manually because distro version is broken
-echo "Installing cloud-init"
-rpm -i http://dl.fedoraproject.org/pub/epel/7/x86_64/c/cloud-init-0.7.5-6.el7.x86_64.rpm
