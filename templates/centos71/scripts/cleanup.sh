8cf05fd1856964bb9b0d2cd2b04ec80bd72430cb
Loek Geleijn <loek@geleijn.nl>
2015-06-22 19:03:50 +0200

    CentOS 7 and RHEL 7 use biosdevname, the 70-persistent-net.rules file is no longer used.

--- templates/centos71/scripts/cleanup.sh
+++ templates/centos71/scripts/cleanup.sh
@@ -8,8 +8,6 @@ yum -y clean all
 echo "Remove DHCP leases"
 rm -f /var/lib/NetworkManager/*.lease
 
-echo "Clean udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
 sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
 
 echo "Remove SSH host keys"
