5679b6cffb398498fb2978139aa7610e111f962a
Lennert den Teuling <lennert@denteuling.com>
2015-06-23 12:04:28 +0200

    Updated CentOS 6.6 template

--- templates/centos66/scripts/base.sh
+++ templates/centos66/scripts/base.sh
@@ -9,11 +9,3 @@ chkconfig --add cloud-set-guest-password
 
 echo "Mount partitions with noatime attribute"
 sed -i '0,/defaults/{s/defaults/defaults,noatime/g}' /etc/fstab
-
-echo "Fixing GRUB"
-cat >>/etc/default/grub <<TOGRUB
-GRUB_RECORDFAIL_TIMEOUT=10
-GRUB_CMDLINE_LINUX_DEFAULT="\$(echo \$GRUB_CMDLINE_LINUX_DEFAULT | sed 's/\(quiet\|splash\|nomodeset\)//g') quiet"
-GRUB_CMDLINE_LINUX="\$(echo \$GRUB_CMDLINE_LINUX | sed 's/\(quiet\|splash\|nomodeset\)//g') nomodeset"
-TOGRUB
-grub2-mkconfig > /boot/grub2/grub.cfg
