76c5882ab4dcd60e58caf6d1c27150b38b07649c
Lennert den Teuling <lennert@denteuling.com>
2015-06-23 12:06:03 +0200

    Updated Debian 7.8 template

---
+++ templates/debian78/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+if [ -d "packer_output" ]; then
+    rm -fr packer_output
+fi
+
+packer build template.json
+
+qemu-img convert -c -f qcow2 -O qcow2 packer_output/debian78.qcow2 debian78_`date +%d-%m-%Y`.qcow2
+
+rm -fr packer_output
