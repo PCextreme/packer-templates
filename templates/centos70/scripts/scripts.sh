2fe240b24fdfdf1589a93d2f0bb66d1272e50847
Lennert den Teuling <lennert@denteuling.com>
2015-06-23 12:11:23 +0200

    Removed CentOS 70 because it is superceeded by CentOS71

--- templates/centos70/scripts/scripts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "installing cloud-set-guest-password"
-chmod +x /usr/lib/systemd/scripts/cloud-set-guest-password
-systemctl enable /etc/systemd/system/cloud-set-guest-password.service
-
-echo "Enable NetworkManager-wait-online" 
-systemctl enable NetworkManager-wait-online.service
