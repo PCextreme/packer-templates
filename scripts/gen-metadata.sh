6dc5af602647750e730e7b0c5444ee92a60c64f9
Wido den Hollander <wido@pcextreme.nl>
2015-07-21 23:01:08 +0200

    Add script for generating metadata

---
+++ scripts/gen-metadata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OUTPUT=$1
+
+if [ -z "$OUTPUT" ]; then
+    echo "Usage: $0 <output file>"
+    exit 1
+fi
+
+GIT_REVISION=$(git rev-parse HEAD)
+DATE=$(date -R)
+PACKER_VERSION=$(packer --version|awk '{print $2}')
+
+echo "# PCextreme Aurora Packer Template" > $OUTPUT
+echo "git revision: $GIT_REVISION" >> $OUTPUT
+echo "build date: $DATE" >> $OUTPUT
+echo "packer version: $PACKER_VERSION" >> $OUTPUT
