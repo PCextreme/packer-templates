50ea6567a1f4f2c5552619938053356311fd8056
Lennert den Teuling <lennert@denteuling.com>
2016-01-18 14:16:07 +0100

    Updated to Fedora Core 23

---
+++ templates/fedora23/scripts/cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+unset HISTFILE
+
+echo "Clean dnf cache"
+dnf -y clean all
+
+echo "Remove DHCP leases"
+rm -f /var/lib/NetworkManager/*.lease
+
+echo "Clean udev rules"
+sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+
+echo "Remove SSH host keys"
+rm -f /etc/ssh/ssh_host*key*
