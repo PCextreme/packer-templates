05be3176dd9b7314971dae3d7696b90fb82dc706
Wido den Hollander <wido@widodh.nl>
2016-02-18 17:04:40 +0100

    ubuntu1510: Do not make the udev rule file a directory
    This causes issues with installing new kernels
    
    Since the generator is also removed we do not have to worry about
    this file being created again

--- templates/ubuntu1510/scripts/cleanup.sh
+++ templates/ubuntu1510/scripts/cleanup.sh
@@ -9,7 +9,6 @@ rm /var/lib/dhcp/*
 
 echo "cleaning up udev rules"
 rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
 rm -rf /dev/.udev/
 rm /lib/udev/rules.d/75-persistent-net-generator.rules
 
