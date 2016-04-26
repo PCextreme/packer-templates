6a62667a2ba2000a12099d6e3506fdbc8ec0e3b0
Wido den Hollander <wido@pcextreme.nl>
2016-04-26 18:16:13 +0200

    zerodisk: Write exactly the amount of free space left
    Use df to find out how much space if left

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 echo "Info: Zero out the free space to save space"
-dd if=/dev/zero of=/EMPTY bs=1M
-sync
-rm -f /EMPTY
+
+AVAIL=$(df --block-size=4K /|tail -1|awk '{print $4}')
+
+dd if=/dev/zero of=/EMPTY conv=fsync bs=4k count=${AVAIL}
+rm /EMPTY
 sync
