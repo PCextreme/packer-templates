de8d81d41d79c81d3ec275422903b5a28fcb3c47
Rene Diepstraten <rene@pcextreme.nl>
2016-07-20 10:59:24 +0200

    Fixes #32 ( SElinux blocking cloud-set-guest-password )

--- templates/fedora24/scripts/scripts.sh
+++ templates/fedora24/scripts/scripts.sh
@@ -6,6 +6,9 @@ echo "installing cloud-set-guest-password"
 chmod +x /usr/lib/systemd/scripts/cloud-set-guest-password
 systemctl enable cloud-set-guest-password
 
+echo "adding SElinux exceptions for cloud-set-guest-password"
+semodule -i /tmp/cloud-password-fix.pp
+
 echo "enable cloud-init"
 systemctl enable cloud-init
 
