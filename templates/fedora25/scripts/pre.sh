162de553cedf58bbfe6fb48742e88ded9b754d3d
Loek Geleijn <loek@pcextreme.nl>
2016-12-30 16:58:24 +0100

    Why?

--- templates/fedora25/scripts/pre.sh
+++ templates/fedora25/scripts/pre.sh
@@ -5,15 +5,7 @@ mkdir -p /usr/lib/systemd/scripts
 
 echo "Symlink dhclient folder to NetworkManager so cloud-init is able to find the leases file"
 rm -rf /var/lib/dhclient
-ln -s /var/lib/NetworkManager /var/lib/dhclient
-
-#echo "Fixing GRUB"
-#cat >>/etc/default/grub <<TOGRUB
-#GRUB_RECORDFAIL_TIMEOUT=10
-#GRUB_CMDLINE_LINUX_DEFAULT="\$(echo \$GRUB_CMDLINE_LINUX_DEFAULT | sed 's/\(quiet\|splash\|nomodeset\)//g') quiet"
-#GRUB_CMDLINE_LINUX="\$(echo \$GRUB_CMDLINE_LINUX | sed 's/\(quiet\|splash\|nomodeset\)//g') nomodeset"
-#TOGRUB
-#grub2-mkconfig > /boot/grub2/grub.cfg
+ln -s /var/lib/NetworkManager /var/lib/dhclients
 
 echo "Do not use IPv4 DHCP DNS"
 sed -i 's|^PEERDNS=yes|PEERDNS=no|g' /etc/sysconfig/network-scripts/ifcfg-*
