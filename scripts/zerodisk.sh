8df2159802aa067c798202d6e2b465814fba3be5
Loek Geleijn <loek@pcextreme.nl>
2017-01-02 10:20:26 +0100

    Speciaal voor Martijn

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -4,5 +4,5 @@ echo "Info: Zero out the free space to save space"
 AVAIL=$(df --block-size=4K /|tail -1|awk '{print $4}')
 
 dd if=/dev/zero of=/EMPTY conv=fsync bs=4k count=${AVAIL}
-rm /EMPTY
+rm -f /EMPTY
 sync
