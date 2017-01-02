6d4e9811a11f2b922365a31fff643f5aec8dbcd9
Loek Geleijn <loek@pcextreme.nl>
2017-01-02 13:46:29 +0100

    This was moved to kickstart script.

--- templates/centos68/scripts/base.sh
+++ templates/centos68/scripts/base.sh
@@ -3,6 +3,3 @@
 echo "Install cloud-set-guest-password"
 chmod +x /etc/init.d/cloud-set-guest-password
 chkconfig --add cloud-set-guest-password
-
-echo "Mount partitions with noatime attribute"
-sed -i '0,/defaults/{s/defaults/defaults,noatime/g}' /etc/fstab
