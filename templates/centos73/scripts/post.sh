89759444f6dac2a78572d1289e967a151d2bcbb0
Loek Geleijn <loek@pcextreme.nl>
2017-01-02 13:51:39 +0100

    Consistency.

--- templates/centos73/scripts/post.sh
+++ templates/centos73/scripts/post.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-unset HISTFILE
+echo "Clean yum cache"
+yum -y clean all
+
+echo "Remove DHCP leases"
+rm -f /var/lib/NetworkManager/*.lease
+
+sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+
+echo "Remove SSH host keys"
+rm -f /etc/ssh/ssh_host*key*
 
 echo "installing cloud-set-guest-password"
 chmod +x /usr/lib/systemd/scripts/cloud-set-guest-password
@@ -8,3 +17,5 @@ systemctl enable cloud-set-guest-password
 
 echo "Enable NetworkManager-wait-online"
 systemctl enable NetworkManager-wait-online.service
+
+unset HISTFILE
