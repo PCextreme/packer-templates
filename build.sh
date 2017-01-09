ddf7f22a062d7d30e0e857b932d3f16a4acff2cf
Wido den Hollander <wido@pcextreme.nl>
2017-01-09 13:27:32 +0100

    Do not use zerodisk script, use fstrim
    By using VirtIO SCSI during the build and discard/unmap in combination
    with fstrim we can trim a QCOW2 easily.
    
    QCOW2 compression is enabled in Packer so that it will trim the disk afterwards.
    
    Signed-off-by: Wido den Hollander <wido@pcextreme.nl>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ TEMPLATES_DIR="${SCRIPT_DIR}/templates"
 # It could be that variables have already been set by Gitlab's CI for example
 # Check if not defined before setting them
 if [ -z "$DISK_SIZE" ]; then
-    DISK_SIZE=3072
+    DISK_SIZE=4096
 fi
 
 if [ -z "$HEADLESS" ]; then
@@ -26,6 +26,8 @@ if [ -z "$MAXPROCS" ]; then
     MAXPROCS=`nproc`
 fi
 
+echo "Building $TEMPLATE_NAME with disk size: $DISK_SIZE"
+
 TEMPLATE_DIR_DISPLAY="templates/$TEMPLATE_NAME"
 RETURN_CODE=0
 
@@ -34,15 +36,3 @@ cd ${TEMPLATES_DIR}/${TEMPLATE_NAME}
 ../../scripts/gen-metadata.sh ../../httpdir/meta.data
 
 packer build -var "disk_size=$DISK_SIZE" -var "ncpu=$MAXPROCS" -var "template_name=$TEMPLATE_NAME" -var "headless=$HEADLESS" template.json
-
-qemu-img convert -q -c -f qcow2 -O qcow2 packer_output/${TEMPLATE_NAME} ${TEMPLATE_NAME}.qcow2
-
-rm -r packer_output
-
-for SIZE in 20G 50G 100G; do
-    qemu-img create -q -f qcow2 ${TEMPLATE_NAME}-${SIZE}.qcow2 ${SIZE}
-
-    #virt-resize --expand /dev/sda2 ${TEMPLATE_NAME}.qcow2 ${TEMPLATE_NAME}-${SIZE}.qcow2
-done
-
-#rm ${TEMPLATE_NAME}.qcow2
--- scripts/zerodisk.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Info: Zero out the free space to save space"
-
-AVAIL=$(df --block-size=4K /|tail -1|awk '{print $4}')
-
-dd if=/dev/zero of=/EMPTY conv=fsync bs=4k count=${AVAIL}
-rm -f /EMPTY
-sync
