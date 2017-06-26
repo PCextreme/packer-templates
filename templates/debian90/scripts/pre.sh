d604d6e9bb93ac8ee20f02d38c5d3c76fc24358e
Wido den Hollander <wido@pcextreme.nl>
2017-06-26 09:40:39 +0200

    Add Debian 9.0 (Stretch) template

---
+++ templates/debian90/scripts/pre.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Fixing GRUB"
+cat >>/etc/default/grub <<TOGRUB
+GRUB_RECORDFAIL_TIMEOUT=10
+GRUB_CMDLINE_LINUX_DEFAULT="\$(echo \$GRUB_CMDLINE_LINUX_DEFAULT | sed 's/\(quiet\|splash\|nomodeset\)//g') quiet"
+GRUB_CMDLINE_LINUX="\$(echo \$GRUB_CMDLINE_LINUX | sed 's/\(quiet\|splash\|nomodeset\)//g') nomodeset"
+TOGRUB
+update-grub2
+
+unset HISTFILE
