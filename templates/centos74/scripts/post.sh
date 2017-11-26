3036ccdc72dc674e84050871a8c89d4fa027a55d
Lennert den Teuling <lennert@denteuling.com>
2017-11-26 22:02:22 +0100

    Enabled fstrim on CentOS 7.4

--- templates/centos74/scripts/post.sh
+++ templates/centos74/scripts/post.sh
@@ -8,7 +8,7 @@ sed -i '/HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-eth0
 echo "Remove SSH host keys"
 rm -f /etc/ssh/ssh_host*key*
 
-echo "Enable services: cloud-init NetworkManager-wait-online"
-systemctl enable cloud-init NetworkManager-wait-online
+echo "Enable services: cloud-init NetworkManager-wait-online fstrim.timer"
+systemctl enable cloud-init NetworkManager-wait-online fstrim.timer
 
 unset HISTFILE
