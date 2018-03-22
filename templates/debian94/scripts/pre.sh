d834c10a12b95071af0452abf405a3c6750ab4fe
Lennert den Teuling <lennert@denteuling.com>
2018-03-22 17:46:29 +0100

    Updated Debian from 9.3 to 9.4 including some improvements in the cloud-init configuration structure.

--- templates/debian94/scripts/pre.sh
+++ templates/debian94/scripts/pre.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo "Ensure everything is up-to-date"
+apt-get update
+apt-get -y upgrade
+
+echo "install cloud-init"
+apt-get install cloud-init cloud-utils -y
 
 echo "Fixing GRUB"
 cat >>/etc/default/grub <<TOGRUB
@@ -8,8 +14,8 @@ GRUB_CMDLINE_LINUX="\$(echo \$GRUB_CMDLINE_LINUX | sed 's/\(quiet\|splash\|nomod
 TOGRUB
 update-grub2
 
-echo "Update cloud.cfg"
-sed -i 's/set-passwords/\[ set-passwords\, always \]/g' /etc/cloud/cloud.cfg
+#echo "Update cloud.cfg"
+#sed -i 's/set-passwords/\[ set-passwords\, always \]/g' /etc/cloud/cloud.cfg
 
 echo "Adding IPv6 configuration to interfaces file"
 IFACE=$(cat /etc/network/interfaces|grep iface|tail -1|awk '{print $2}')
@@ -23,7 +29,8 @@ echo "Ensure ens3 is used instead of ens4 in interfaces file"
 sed -i 's/ens4/ens3/g' /etc/network/interfaces
 
 echo "Enable fstrim on a weekly basis"
-cp /usr/share/doc/util-linux/examples/fstrim.{service,timer} /etc/systemd/system
+cp /usr/share/doc/util-linux/examples/fstrim.service /etc/systemd/system
+cp /usr/share/doc/util-linux/examples/fstrim.timer /etc/systemd/system
 systemctl enable fstrim.timer
 
 unset HISTFILE
