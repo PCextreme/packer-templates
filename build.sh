9ce284f51d7aa816bbe5828b31b9074879f20f29
Remy Adriaanse <remy@pcextreme.nl>
2020-02-12 14:33:54 +0100

    Base refactor repo layout
    Remove all non-specific template stuff and cleanup.
    This prepares the repo for the templates refactor

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 TEMPLATE_NAME=$1
 
@@ -7,32 +8,6 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-set -ex
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-TEMPLATES_DIR="${SCRIPT_DIR}/templates"
-
-# It could be that variables have already been set by Gitlab's CI for example
-# Check if not defined before setting them
-if [ -z "$DISK_SIZE" ]; then
-    DISK_SIZE=4096
-fi
-
-if [ -z "$HEADLESS" ]; then
-    HEADLESS=1
-fi
-
-if [ -z "$MAXPROCS" ]; then
-    MAXPROCS=`nproc`
-fi
-
-echo "Building $TEMPLATE_NAME with disk size: $DISK_SIZE"
-
-TEMPLATE_DIR_DISPLAY="templates/$TEMPLATE_NAME"
-RETURN_CODE=0
-
-cd ${TEMPLATES_DIR}/${TEMPLATE_NAME}
-
-../../scripts/gen-metadata.sh ../../httpdir/meta.data
-
-packer build -var "disk_size=$DISK_SIZE" -var "ncpu=$MAXPROCS" -var "template_name=$TEMPLATE_NAME" -var "headless=$HEADLESS" template.json
+packer build -var-file=templates/${TEMPLATE_NAME}.json \
+	-var "name=${TEMPLATE_NAME}" \
+	templates/base.json
