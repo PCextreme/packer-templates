eee1c83d9e839de95e60132543c5985258014c2b
Wido den Hollander <wido@pcextreme.nl>
2020-11-02 16:47:42 +0100

    Ubuntu 20.04: Build using cloud-init instead of dist-upgrade
    The new installer from Ubuntu is cloud-init based.
    
    We serve user-data and meta-data over HTTP to the Ubuntu 20.04 ISO
    which then performs the installation.
    
    Some hacking was needed to get the user 'root' to work, but it works
    now.

--- scripts/ubuntu-20.04/post.sh
+++ scripts/ubuntu-20.04/post.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
+
+echo "Installing and updating needed packages"
+apt-get update
+apt-get -o Dpkg::Options::="--force-confold" -y install \
+    qemu-guest-agent ndisc6 cloud-init at byobu curl fping git htop iftop iotop \
+    ipset jq mc mtr ncdu nmap rsync screen strace tcpdump unzip util-linux whois \
+    uuid wget vim software-properties-common sysstat rdnssd watchdog
+apt-get -y dist-upgrade
+
+echo "Enabling services"
+systemctl enable cloud-init.service fstrim.timer qemu-guest-agent.service watchdog.service
+
+echo "Disabling apt timers"
+systemctl disable apt-daily.timer apt-daily-upgrade.timer
+
 echo "Removing uneeded packages"
 apt-get -y autoremove
 apt-get -y clean
 
 echo "cleaning up dhcp leases"
-find /var/lib/dhcp -type f -delete
+find /var/lib -type f -name '*.lease' -print -delete
+
+echo "Configurating network interface"
+find /etc/netplan -type f -name '00-installer-config.yaml' -print -delete
 
 echo "cleaning up udev rules"
 rm -f /etc/udev/rules.d/70-persistent-net.rules
 
 echo "Remove SSH host keys"
-find /etc/ssh -type f -name 'ssh_host*key*' -delete
+find /etc/ssh -type f -name 'ssh_host*key*' -print -delete
 
 echo "cleaning up log files"
 if [ -f /var/log/audit/audit.log ]; then cat /dev/null > /var/log/audit/audit.log; fi
@@ -21,5 +39,12 @@ rm -f /var/log/upstart/*.log /var/log/upstart/*.log.*.gz
 
 echo "Cleaning up cloud-init"
 cloud-init clean -s -l
+find /etc/cloud/cloud.cfg.d/ -type f ! -name '05_logging.cfg' -print -delete
+find /var/log/ -type f -name 'cloud-init*.log' -print -delete
+
+echo "Removing default user ubuntu"
+deluser ubuntu
 
 unset HISTFILE
+
+sync
