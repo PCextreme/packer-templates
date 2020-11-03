97df95664c04d9b1d339e1b249157c6ea70aac56
Wido den Hollander <wido@pcextreme.nl>
2020-11-03 10:51:06 +0100

    Each template has it's own post.sh script
    With each template having it's own post.sh script we can fine-tune
    things we want to do per template.
    
    We now also force the DNS to be set to the IPv6 addresses of the
    PCextreme DNS servers.

---
+++ scripts/centos-8.2/post.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Remove DHCP leases"
+find /var/lib/NetworkManager -type f -name '*.lease'
+
+echo "Remove SSH host keys"
+find /etc/ssh -type f -name 'ssh_host*key*' -delete
+
+echo "Configurating network interface"
+mv /etc/sysconfig/network-scripts/ifcfg-ens4 /etc/sysconfig/network-scripts/ifcfg-ens3
+sed -i 's|ens[0-9]|ens3|g' /etc/sysconfig/network-scripts/ifcfg-ens3
+
+echo "Configuring DNS"
+find /etc -maxdepth 1 -type l -name 'resolv.conf' -delete
+echo "nameserver 2a00:f10:ff04:153::53"|tee /etc/resolv.conf
+echo "nameserver 2a00:f10:ff04:253::53"|tee -a /etc/resolv.conf
+
+echo "Enable services: cloud-init NetworkManager-wait-online fstrim.timer"
+systemctl enable cloud-init cloud-config fstrim.timer qemu-guest-agent
+
+echo "Cleaning up cloud-init"
+cloud-init clean --logs
+
+unset HISTFILE
+
+sync
---
+++ scripts/ubuntu-16.04/post.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "Removing uneeded packages"
+apt-get -y autoremove
+apt-get -y clean
+
+echo "cleaning up dhcp leases"
+find /var/lib/dhcp -type f -delete
+
+echo "Remove SSH host keys"
+find /etc/ssh -type f -name 'ssh_host*key*' -delete
+
+echo "cleaning up udev rules"
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+
+echo "Configuring network interface"
+sed -i 's|ens[0-9]|ens3|g' /etc/network/interfaces
+echo "iface ens3 inet6 auto" >> /etc/network/interfaces
+
+echo "Configuring DNS"
+echo "nameserver 2a00:f10:ff04:153::53"|tee -a /etc/resolvconf/resolv.conf.d/head
+echo "nameserver 2a00:f10:ff04:253::53"|tee -a /etc/resolvconf/resolv.conf.d/head
+
+echo "cleaning up log files"
+if [ -f /var/log/audit/audit.log ]; then cat /dev/null > /var/log/audit/audit.log; fi
+cat /dev/null > /var/log/wtmp 2>/dev/null
+logrotate -f /etc/logrotate.conf 2>/dev/null
+rm -f /var/log/*-* /var/log/*.gz 2>/dev/null
+rm -f /var/log/upstart/*.log /var/log/upstart/*.log.*.gz
+
+echo "Cleaning up cloud-init"
+cloud-init clean --logs
+
+unset HISTFILE
+
+sync
