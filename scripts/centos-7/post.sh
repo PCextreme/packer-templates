d9f26c5d87828d79eb25fe710408fc349fa6f7e7
Robert Selaru <robert@pcextreme.nl>
2021-02-04 15:29:01 +0100

    password auth default yes on centos

--- scripts/centos-7/post.sh
+++ scripts/centos-7/post.sh
@@ -11,6 +11,9 @@ find /etc -maxdepth 1 -type l -name 'resolv.conf' -delete
 echo "nameserver 2a00:f10:ff04:153::53"|tee /etc/resolv.conf
 echo "nameserver 2a00:f10:ff04:253::53"|tee -a /etc/resolv.conf
 
+echo "Enable ssh password auth by default"
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/'g /etc/ssh/sshd_config
+
 echo "Addding timeout to cloud-init"
 mkdir /etc/systemd/system/cloud-init.service.d
 echo -e "[Service]\nTimeoutSec=10" > /etc/systemd/system/cloud-init.service.d/override.conf
