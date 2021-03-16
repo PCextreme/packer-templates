47b37a133f5d9dc098ce997ef6b8947179898621
Wido den Hollander <wido@pcextreme.nl>
2021-03-16 15:08:39 +0100

    Initial stab at CloudLinux 8 template

---
+++ scripts/cloudlinux-8/post.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Remove DHCP leases"
+find /var/lib -type f -name '*.lease' -print -delete
+
+echo "Remove SSH host keys"
+find /etc/ssh -type f -name 'ssh_host*key*' -print -delete
+
+echo "Configurating network interface"
+mv /etc/sysconfig/network-scripts/ifcfg-ens4 /etc/sysconfig/network-scripts/ifcfg-ens3
+sed -i 's|ens[0-9]|ens3|g' /etc/sysconfig/network-scripts/ifcfg-ens3
+
+echo "Configuring DNS"
+find /etc -maxdepth 1 -type l -name 'resolv.conf' -print -delete
+echo "nameserver 2a00:f10:ff04:153::53"|tee /etc/resolv.conf
+echo "nameserver 2a00:f10:ff04:253::53"|tee -a /etc/resolv.conf
+
+echo "Enable services: cloud-init NetworkManager-wait-online fstrim.timer"
+systemctl enable cloud-init cloud-config fstrim.timer qemu-guest-agent
+
+echo "Generating GRUB"
+grub2-mkconfig -o /boot/grub2/grub.cfg
+
+echo "Cleaning up cloud-init"
+find /var/log -type f -name 'cloud-init*.log' -print -delete
+cloud-init clean -s -l
+
+unset HISTFILE
+
+sync
